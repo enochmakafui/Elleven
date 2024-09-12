fac5aacaa820e32db6772cead4baf96c7022021d
enochmack04@gmail.com <enochmack04@gmail.com>
2024-09-12 11:53:52 +0000

    checking hover on mobile

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -9,6 +9,12 @@ import ButtonDark from "./ButtonDark";
 const MainNavigation = () => {
   const [isExpanded, setExpanded] = useState(false);
   const [scrolled, setScrolled] = useState(false);
+  // mobile scroll for small svg
+
+  // haburger menu
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+  const [burger_menu_class, setBurgerMenuClass] = useState("burger-menu");
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   const dropDownYes = () => {
     setExpanded(true);
@@ -25,6 +31,18 @@ const MainNavigation = () => {
     }
   };
 
+  const handleToggleBurgerMenu = () => {
+    if (burger_class === "burger-bar unclicked") {
+      setBurgerClass("burger-bar clicked");
+      setBurgerMenuClass("burger-menu clicked");
+      setMenuOpen(true);
+    } else {
+      setBurgerClass("burger-bar unclicked");
+      setMenuOpen(false);
+      setBurgerMenuClass("burger-menu");
+    }
+  };
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => {
@@ -37,7 +55,7 @@ const MainNavigation = () => {
       <h1>
         <NavLink to="/"> Elleven</NavLink>
       </h1>
-      <ul>
+      <ul className={isMenuOpen ? "menu-open" : ""}>
         <li>
           <NavLink className="navlinks">Our Story </NavLink>
         </li>
@@ -56,12 +74,12 @@ const MainNavigation = () => {
                 height="25"
                 viewBox="0 0 24 25"
                 fill="none"
-                className= "dropdown-icon"
+                className="dropdown-icon"
                 animate={{ rotate: isExpanded ? 180 : 0 }}
               >
                 <path
                   d="M18 10.5L12 16.5L6 10.5"
-                  stroke={scrolled?"black":"white"}
+                  stroke={scrolled ? "black" : "white"}
                   strokeWidth="1.5"
                   strokeLinecap="round"
                   strokeLinejoin="round"
@@ -71,6 +89,9 @@ const MainNavigation = () => {
             <AnimatePresence>{isExpanded && <DropItem />}</AnimatePresence>
           </NavLink>
         </li>
+        {/* <li>
+          <DropItem />
+        </li> */}
         <li>
           <NavLink className="navlinks">Store </NavLink>
         </li>
@@ -83,23 +104,28 @@ const MainNavigation = () => {
               <ButtonDark
                 style={{
                   // padding: "10px 25px",
-                  fontSize:"20px"
+                  fontSize: "20px",
                 }}
               >
                 Start a Project
               </ButtonDark>
             ) : (
               <ButtonLight
-              style={{
-                // padding: "10px 25px",
-                fontSize:"20px"
-              }}>
+                style={{
+                  // padding: "10px 25px",
+                  fontSize: "20px",
+                }}
+              >
                 Start a Project
               </ButtonLight>
             )}
           </NavLink>
         </li>
       </ul>
+      <div className={burger_menu_class} onClick={handleToggleBurgerMenu}>
+        <div className={burger_class}></div>
+        <div className={burger_class}></div>
+      </div>
     </nav>
   );
 };
