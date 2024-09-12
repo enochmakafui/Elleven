3149528a66f14c2f0253f201196f5f3352cc89b2
enochmack04@gmail.com <enochmack04@gmail.com>
2024-09-12 23:51:04 +0000

    build mobile view up to insights

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { motion, useInView } from "framer-motion";
 import carAnimationVideo from "../assets/Videos/cal2careBrand_videos/car.mp4";
 import "../CSS/ClientSection.css";
+import VideoBox from "../components/VideoBox";
 const Service = () => {
   const h2Ref = useRef(null);
   const cardRef = useRef(null);
@@ -46,11 +47,12 @@ const Service = () => {
         animate={cardVisible ? "visible" : "hidden"}
         variants={variants}
         transition={{ duration: 0.5 }}
-        className="service-img-container"
+        // className="service-img-container"
       >
-        <video autoPlay loop muted>
+        {/* <video autoPlay loop muted>
         <source src={carAnimationVideo} type="video/mp4" />
-      </video>
+      </video> */}
+        <VideoBox videoSrc={carAnimationVideo}/>
       </motion.div>
     </div>
   );
